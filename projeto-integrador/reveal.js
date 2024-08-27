c4e94b3426084c56b9111700fea83dc66a560bab
Emanuel Lira <emanuelll.13141@gmail.com>
2024-08-27 10:55:44 -0300

    Add files via upload

---
+++ projeto-integrador/reveal.js
@@ -0,0 +1,32 @@
+window.revelar = ScrollReveal({ reset: false });
+
+revelar.reveal('.efeito-txt-topo', {
+    duration: 2000,
+    distance: '90px'
+});
+revelar.reveal('.lua', {
+    duration: 2000,
+    distance: '90px',
+    delay:500,
+    origin: 'right'
+});
+
+revelar.reveal('.efeito-p-topo', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+});
+
+revelar.reveal('.content-sobre', 
+  {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+});
+
+revelar.reveal('.card', 
+  {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+});
